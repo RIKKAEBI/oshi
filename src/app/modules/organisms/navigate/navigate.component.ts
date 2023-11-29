df88e24a3222d25ecb1f6298e6162163328ef2cb
RIKKAEBI <kazuki.jar@gmail.com>
2023-11-29 20:17:01 +0900

    👍 navigate を追加

--- src/app/modules/organisms/navigate/navigate.component.ts
+++ src/app/modules/organisms/navigate/navigate.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IconConfigComponent } from '../../atoms/icon-config/icon-config.component';
+import { IconHomeComponent } from '../../atoms/icon-home/icon-home.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigate',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    IconConfigComponent,
+    IconHomeComponent,
+  ],
   templateUrl: './navigate.component.html',
   styleUrl: './navigate.component.css'
 })
 export class NavigateComponent {
-  
+  constructor (
+    private router: Router
+  ) {}
+
+  onNavigate ($even: MouseEvent, path: string): void {
+    $even.preventDefault()
+    this.router.navigateByUrl(path)
+  }
 }
