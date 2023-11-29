946845b65a3eb76419b0f36dbaaa2e84a52e6fe0
RIKKAEBI <kazuki.jar@gmail.com>
2023-11-29 19:27:11 +0900

    👍 navigate コンポーネントの作成

---
+++ src/app/modules/organisms/navigate/navigate.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navigate',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navigate.component.html',
+  styleUrl: './navigate.component.css'
+})
+export class NavigateComponent {
+  
+}
