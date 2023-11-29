9809d03e2d3871f348e53f840455207dd8c0537e
RIKKAEBI <kazuki.jar@gmail.com>
2023-11-29 19:27:57 +0900

    🎨 アプリの layout を作成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FirebaseService } from './service/firebase.service';
+import { NavigateComponent } from './modules/organisms/navigate/navigate.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavigateComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
