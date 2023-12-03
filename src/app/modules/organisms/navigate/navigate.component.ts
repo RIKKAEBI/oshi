73c8cee1ead3f5c22efcd9e29a68cd2a866f05ca
RIKKAEBI <kazuki.jar@gmail.com>
2023-12-03 16:55:28 +0900

    🔥 icon の削除

--- src/app/modules/organisms/navigate/navigate.component.ts
+++ src/app/modules/organisms/navigate/navigate.component.ts
@@ -1,26 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IconHomeComponent } from '../../atoms/icon-home/icon-home.component';
 import { Router } from '@angular/router';
-import { IconSettingsComponent } from '../../atoms/icon-settings/icon-settings.component';
 
 @Component({
   selector: 'app-navigate',
   standalone: true,
-  imports: [
-    CommonModule,
-    IconSettingsComponent,
-    IconHomeComponent,
-  ],
+  imports: [CommonModule],
   templateUrl: './navigate.component.html',
   styleUrl: './navigate.component.css'
 })
 export class NavigateComponent {
-  constructor (
+  constructor(
     private router: Router
-  ) {}
+  ) { }
 
-  onNavigate ($even: MouseEvent, path: string): void {
+  onNavigate($even: MouseEvent, path: string): void {
     $even.preventDefault()
     this.router.navigateByUrl(path)
   }
