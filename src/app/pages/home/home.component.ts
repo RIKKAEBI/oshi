6b211c9dbfcd0c6159cd97f723b1febddf6f7591
RIKKAEBI <kazuki.jar@gmail.com>
2023-12-03 17:09:43 +0900

    🎨 リストフレームの作成

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { MoneyService } from '../../service/money.service';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,14 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  list: number[] = [...Array(100).keys()].map(i => i + 1)
 
+  constructor (
+    private moneyService: MoneyService
+  ) { }
+
+  ngOnInit (): void {
+    this.moneyService.initializeStorage()
+  }
 }
